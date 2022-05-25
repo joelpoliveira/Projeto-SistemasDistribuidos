f60e0f68e2c322db75f3afc61eae069b4eefe4d5
Joel Oliveira <58565967+joelpoliveira@users.noreply.github.com>
2022-05-25 16:56:13 +0100

    seleção de equipas e jogadores

--- TP2/sd/src/main/java/projeto/sd/controller/HomeController.java
+++ TP2/sd/src/main/java/projeto/sd/controller/HomeController.java
@@ -9,6 +9,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
+import java.time.LocalDateTime;
 
 import javax.servlet.http.HttpSession;
 
@@ -111,9 +112,9 @@ public class HomeController {
             playerService.add(new Player("D", "Striker", null, t));
         }
 
-        gameService.add(new Game("Lisboa", null, teams.get(0), teams.get(1)));
-        gameService.add(new Game("Lisboa", null, teams.get(0), teams.get(2)));
-        gameService.add(new Game("Lisboa", null, teams.get(3), teams.get(1)));
+        gameService.add(new Game("Lisboa", LocalDateTime.now(), teams.get(0), teams.get(1)));
+        gameService.add(new Game("Lisboa", LocalDateTime.now(), teams.get(0), teams.get(2)));
+        gameService.add(new Game("Lisboa", LocalDateTime.now()  , teams.get(3), teams.get(1)));
 
         return "redirect:/";
     }
